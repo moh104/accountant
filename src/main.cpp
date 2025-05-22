93055a2ba214654b00ad5d0900d5b2374759529a
moh104 <mg1game1m@gmail.com>
2025-05-22 22:10:31 +0330

    Add main function

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "currency.hpp"
+#include "IRR.hpp"
+#include "EUR.hpp"
+#include "USD.hpp"
+#include "currency_operators.hpp"
+#include "bankaccount.hpp"
+#include "program.hpp"
+
+using namespace std;
+
+int main()
+{
+    Program program;
+    program.run();
+    return 0;
+}
